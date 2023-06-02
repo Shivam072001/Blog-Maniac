7b4d375fa89a84aa188d0a747666f390048b20aa
Shivam072001 <87487649+Shivam072001@users.noreply.github.com>
2023-06-02 14:26:02 +0530

    Update Firebase-admin.js

--- Firebase/Firebase-admin.js
+++ Firebase/Firebase-admin.js
@@ -4,8 +4,10 @@ if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       client_email: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n"): undefined,
-      project_id: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
+        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n")
+        : undefined,
+      project_id: "swagger-3360e",
     }),
     databaseURL: process.env.FIREBASE_DATABASE_URL,
   });
