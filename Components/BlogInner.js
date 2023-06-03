ee560446d546ba22f8c6d7736aaf8d0c15622a5b
Shivam072001 <87487649+Shivam072001@users.noreply.github.com>
2023-06-03 13:32:20 +0530

    Update BlogInner.js

--- Components/BlogInner.js
+++ Components/BlogInner.js
@@ -10,7 +10,7 @@ function BlogInner({ data, content, headings }) {
         style={{ width: "calc(100% - 15rem)" }}
       >
         <img
-          className="object-cover w-full h-72"
+          className="object-cover w-full h-full"
           src={data.HeaderImage}
           alt="Article Image"
         />
